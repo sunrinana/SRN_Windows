07b7ecdb85cfa3a791b335cf70b9290b1384b9a7
YonghoAhn <hrh1110@naver.com>
2016-10-29 13:24:45 +0900

    AutoSend

--- SRN_Client/Form1.cs
+++ SRN_Client/Form1.cs
@@ -15,31 +15,44 @@ namespace SRN_Client
 {
     public partial class Form1 : Form
     {
+        private int time = 0; //500ms마다 한번씩으로 카운트
         private SerialPort arduSerialPort = new SerialPort();
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void send_server()
         {
-            HttpWebRequest register_request = (HttpWebRequest)WebRequest.Create("http://working.anapp.kr:3000/send ");
-            int postData = 1;
-            // postData = "drop database;";//JsonConvert.SerializeObject(u);
-            var data = Encoding.ASCII.GetBytes(postData.ToString());
-            register_request.Method = "POST";
-            register_request.ContentType = "application/x-www-form-urlencoded";
+            /*
+             * 서버에 POST 메세지로
+             * 일 안할때만 no work
+             * 뭐 이딴식으로 보낸다
+             * 일할때는 보낼 필요도
+             * 없을 듯 하다.
+             */
+            var request = (HttpWebRequest)WebRequest.Create("http://working.anapp.kr/send");
+            var postData = "sleep";
+            var data = Encoding.ASCII.GetBytes(postData);
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
 
-            register_request.ContentLength = data.Length;
-            using (var stream = register_request.GetRequestStream())
+            request.ContentLength = data.Length;
+            using (var stream = request.GetRequestStream())
             {
                 stream.Write(data, 0, data.Length);
             }
+            Console.WriteLine("Send");
+        }
 
-            var response = (HttpWebResponse)register_request.GetResponse();
-            string responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+        private void button1_Click(object sender, EventArgs e)
+        {
 
-            MessageBox.Show(responseString);
+
+            //var response = (HttpWebResponse)register_request.GetResponse();
+            //string responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+            //MessageBox.Show(responseString);
             
         }
 
@@ -52,7 +65,30 @@ namespace SRN_Client
 
         private void timer1_Tick(object sender, EventArgs e)
         {
+            /*
+             * TODO : cnt는 0.5초마다 한번씩 작동,
+             * 따라서 1초에 두번 읽어온다.
+             * 그렇가면 if((cnt / 2) == 60)인 조건에서 일을 안 한다고 가정하고 전송한다
+             * 
+             */
+             
             string input = arduSerialPort.ReadLine();
+            //Console.WriteLine(input);
+            if(input[0] == '0')
+            {
+                //움직임 없으면
+                time+=1;
+                Console.WriteLine("+++");
+            }
+            else
+            {
+                time = 0;
+            }
+            if(time == 30)
+            {
+                Console.WriteLine("SEX");
+                send_server();
+            }
             textBox1.Text = input + "\n";
         }
     }
